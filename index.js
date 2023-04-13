092eecbe97da79f8b0f2e6c2bb8fb486553bdc94
Shania Lopez <slopez@fandm.edu>
2023-04-13 06:42:21 -0400

    complete lab again

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sherlock",
+    address: "221B Baker Street"
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+    const newEmployee = {...employee}
+    newEmployee[key] = value;
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+   
+   const newEmployee = {...employee};
+   newEmployee[key];
+   delete newEmployee.name;
+   return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    employee[key];
+    delete employee.name;
+    return employee;
+
+}
